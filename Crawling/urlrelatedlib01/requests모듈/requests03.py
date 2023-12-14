ba54fda6666cb72c54194cffb8853d11fe983283
Seung <esybd02@naver.com>
2023-12-14 09:20:37 +0900

    가상환경 설정

---
+++ Crawling/urlrelatedlib01/requests모듈/requests03.py
@@ -0,0 +1,7 @@
+'''
+[외부 REST API 요청 (JSONPlaceHolder)]
+https://jsonplaceholder.typicode.com/photos
+- 위의 url로 요청 보내기 -> json 형태의 문자열이 반환됨
+- json() : requests 모듈에 내장된, json형태의 문자열을 파이썬의 리스트 형태로 디코딩해주는 함수
+    -> import json 불필요
+'''
