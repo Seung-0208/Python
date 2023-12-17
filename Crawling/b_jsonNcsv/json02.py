7cfdf3764af1937eb99d8bbaed0ea23d0dee20f8
Seung <esybd02@naver.com>
2023-12-18 02:19:18 +0900

    json모듈의 함수를 사용해 jsonplaceholder에서 유저 데이터 받기

--- Crawling/b_jsonNcsv/json02.py
+++ Crawling/b_jsonNcsv/json02.py
@@ -10,3 +10,20 @@ import  requests
 import  json
 
 res = requests.get('https://jsonplaceholder.typicode.com/users')
+print(type(res.text)) #json 형태의 문자열 반환
+
+#json모듈의 loads() 함수 사용
+users = json.loads(res.text)
+print(users) #dict타입의 요소를 갖는 list 반환
+for user in users: #정리해서 출력
+    for key, value in user.items():
+        print(f'{key}:{value}', end='\t\t')
+    print()
+
+#requests에 내장된 json() 함수 사용
+users = res.json()
+print(users) #dict타입의 요소를 갖는 list 반환
+for user in users: #정리해서 출력
+    for key, value in user.items():
+        print(f'{key}:{value}', end='\t\t')
+    print()
